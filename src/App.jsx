e2c458ad5366044608a4d7ef1f4af84c3941d2f8
Santoshshriwas <santoshdhobi9589@gmail.com>
2024-03-30 11:31:24 +0530

    2 Un complate

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Cart from './Pages/Cart/Cart';
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder';
 import Home from './Pages/Home/Home';
 import Footer from './Components/Footer/Footer';
+import LoginPopup from './Components/LoginPopup/LoginPopup';
 
 const App = () => {
+
+  const [showLogin,setShowLogin] =useState(false)
   return (
     <div className='app'>
       <>
-      <Navbar/>
+      {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      <Navbar setShowLogin={setShowLogin}/>
        <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
